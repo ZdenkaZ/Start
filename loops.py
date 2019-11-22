b3a06044cf87ae81b957d8d358ed4c5f999385fa
ZdenkaZ <zdenkaherova@gmail.com>
2019-11-22 22:46:07 +0100

    first code

---
+++ loops.py
@@ -0,0 +1,30 @@
+# counter = 0
+# while counter < 10:
+#   if counter % 2 == 0:
+#     print(x)
+#   counter += 1
+
+# For Loop
+
+a = [1,4,6,4,3,2,2]
+
+# counter = 0
+# while counter < len(a):
+#   print(a[counter])
+#   counter += 1
+
+# for item in a:
+#   print(item)
+
+# b = []
+
+# for x in a:
+#   if x % 2 == 0:
+#     b.append(x * 5)
+#   print(b)
+
+
+b = [print(x) for x in a]
+# print(b)
+
+print(b)
